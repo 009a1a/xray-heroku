ab31f352420f39a392ae99f56e81453492eec85e
Trumpy <91765499+Trumpy@users.noreply.github.com>
2021-10-25 00:08:54 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,15 +67,15 @@ unzip Xray.zip
 rm -rf Xray.zip
 chmod +x Xray
 
-C_VER="v1.0.3"
-mkdir /caddybin
-cd /caddybin
-CADDY_URL="https://github.com/caddyserver/caddy/releases/download/$C_VER/caddy_${C_VER}_linux_amd64.tar.gz"
-echo ${CADDY_URL}
-wget --no-check-certificate -qO 'caddy.tar.gz' ${CADDY_URL}
-tar xvf caddy.tar.gz
-rm -rf caddy.tar.gz
-chmod +x caddy
+N_VER="v1.20.1"
+mkdir /nginxbin
+cd /nginxbin
+CADDY_URL="https://nginx.org/download/nginx-1.20.1.tar.gz"
+echo ${NGINX_URL}
+wget --no-check-certificate -qO 'nginx.tar.gz' ${NGINX_URL}
+tar xvf nginx.tar.gz
+rm -rf nginx.tar.gz
+chmod +x nginx
 
 cd /wwwroot
 tar xvf wwwroot.tar.gz

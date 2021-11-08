be527b8f95e1af2780da8c7f5ae2d0f55544ee38
Trumpy <91765499+Trumpy@users.noreply.github.com>
2021-11-08 06:48:19 +0000

    Update entrypoints.sh

--- entrypoints.sh
+++ entrypoints.sh
@@ -61,6 +61,45 @@ cd /wwwroot
 tar xvf wwwroot.tar.gz
 rm -rf wwwroot.tar.gz
 
+sed -e "/^#/d"\
+    -e "s/\${Vless_UUID}/${Vless_UUID}/g"\
+    -e "s|\${Vless_Path}|${Vless_Path}|g"\
+    -e "s/\${Vmess_UUID}/${Vmess_UUID}/g"\
+    -e "s|\${Vmess_Path}|${Vmess_Path}|g"\
     /conf/Xray.template.json >  /xraybin/config.json
 echo /xraybin/config.json
 cat /xraybin/config.json
+
+if [[ -z "${ProxySite}" ]]; then
+  s="s/proxy_pass/#proxy_pass/g"
+  echo "site:use local wwwroot html"
+else
+  s="s|\${ProxySite}|${ProxySite}|g"
+  echo "site: ${ProxySite}"
+fi
+
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${Vless_Path}|${Vless_Path}|g"\
+    -e "s|\${Vmess_Path}|${Vmess_Path}|g"\
+    -e "s|\${Share_Path}|${Share_Path}|g"\
+    -e "$s"\
+    /conf/nginx.template.conf > /etc/nginx/conf.d/ray.conf
+echo /etc/nginx/conf.d/ray.conf
+cat /etc/nginx/conf.d/ray.conf
+
+[ ! -d /wwwroot/${Share_Path} ] && mkdir -p /wwwroot/${Share_Path}
+sed -e "/^#/d"\
+    -e "s|\${_Vless_Path}|${Vless_Path}|g"\
+    -e "s|\${_Vmess_Path}|${Vmess_Path}|g"\
+    -e "s/\${_Vless_UUID}/${Vless_UUID}/g"\
+    -e "s/\${_Vmess_UUID}/${Vmess_UUID}/g"\
+    -e "$s"\
+    /conf/share.html > /wwwroot/${Share_Path}/index.html
+echo /wwwroot/${Share_Path}/index.html
+cat /wwwroot/${Share_Path}/index.html
+
+cd /xraybin
+./xray run -c ./config.json &
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'

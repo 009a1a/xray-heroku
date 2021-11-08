e5ac8a0480e27c8070945234858b7dbaade55a7b
Trumpy <91765499+Trumpy@users.noreply.github.com>
2021-11-08 07:40:37 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,27 +35,20 @@ ln -sf /usr/share/zoneinfo/Europe/UnitedKingdom/etc/localtime
 date -R
 
 if [ "$VER" = "latest" ]; then
-  XRAY_URL="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-32.zip"
+  VER=`wget -qO- "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | sed -n -r -e 's/.*"tag_name".+?"([vV0-9\.]+?)".*/\1/p'`
+  [[ -z "${VER}" ]] && VER="v1.2.2"
 else
   VER="v$VER"
-  XRAY_URL="https://github.com/XTLS/Xray-core/releases/download/${VER}/Xray-linux-32.zip"
 fi
 
-if [ "$VER" = "latest" ]; then
-  XRAY_URL="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip"
-else
-  VER="v$VER"
-  XRAY_URL="https://github.com/XTLS/Xray-core/releases/download/${VER}/Xray-linux-64.zip"
-fi
-
-VER="latest"
-mkdir /Xraybin
-cd /Xraybin
-echo ${XRAY_URL}
-wget --no-check-certificate -qO 'Xray.zip' ${XRAY_URL}
-unzip Xray.zip
-rm -rf Xray.zip
-chmod +x Xray
+mkdir /xraybin
+cd /xraybin
+RAY_URL="https://github.com/XTLS/Xray-core/releases/download/${VER}/Xray-linux-64.zip"
+echo ${RAY_URL}
+wget --no-check-certificate ${RAY_URL}
+unzip Xray-linux-64.zip
+rm -f Xray-linux-64.zip
+chmod +x ./xray
 
 cd /wwwroot
 tar xvf wwwroot.tar.gz

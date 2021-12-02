caaa7e2e3d4e5e6d2f6ed580f7d1ab7afbcac5d3
Trumpy <91765499+Trumpy@users.noreply.github.com>
2021-12-02 16:42:54 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -97,7 +97,7 @@ cat <<-EOF > /v2raybin/config.json
             ]
         },
         "streamSettings":{
-            "security": "chacha20-ietf-poly1305",
+            "security": "xchacha20-ietf-poly1305",
             "network":"ws",
             "wsSettings":{
                 "path": "${V2_Path}"
@@ -141,7 +141,7 @@ cat <<-EOF > /v2raybin/vmess.json
     "port": "443",
     "id": "${UUID}",
     "aid": "${AlterID}",
-    "security": "chacha20-ietf-poly1305",
+    "security": "xchacha20-ietf-poly1305",
     "net": "ws",
     "type": "none",
     "host": "",

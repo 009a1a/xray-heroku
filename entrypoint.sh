3b969ade9239b747926d558c713d9e7ced8cd35f
Trumpy <91765499+Trumpy@users.noreply.github.com>
2021-12-02 19:02:26 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -100,7 +100,7 @@ cat <<-EOF > /v2raybin/config.json
             "security": "xchacha20-ietf-poly1305",
             "network": "ws",
             "wsSettings": {
-                "host": "",
+                "host": "${V2_Host}",
 		"path": "${V2_Path}"
             }
         }
